a51eafc820c879c455877751eeb1f974360698fd
chouakifares <fareschoauki2015@gmail.com>
2021-12-10 00:06:09 +0100

    added complete some and deducte all predicates

--- test.pl
+++ test.pl
@@ -7,7 +7,10 @@ prog :-
 	deuxieme_etape(Abi,Abi1,Tbox),
 	troisieme_etape(Abi1,Abr).
 
-premiere_etape(Tbox,Abi,Abr) :- setof((X,Y), equiv(X,Y),Tbox),setof((X,Y), inst(X,Y),Abi),setof((X,Y,Z), instR(X,Y,Z),Abr).
+premiere_etape(Tbox,Abi,Abr) :-
+	setof((X,Y), equiv(X,Y),Tbox),
+	setof(inst(X,Y), inst(X,Y),Abi),
+	setof((X,Y,Z), instR(X,Y,Z),Abr).
 
 deuxieme_etape(Abi,Abi1,Tbox) :-
 	saisie_et_traitement_prop_a_demontrer(Abi,Abi1,Tbox).
@@ -64,15 +67,15 @@ part_1_acquisition_prop_type2(Abi,Abi1,Tbox) :-
 	recc_replace(C1,C1_transformed),
 	validate_concept(C1_transformed),
 	part_2_acquisition_prop_type2(C1_transformed,Abi,Abi1,Tbox).
-part_2_acquisition_prop_type2(C1,Abi,Abi1,Tbox) :-
+part_2_acquisition_prop_type2(C1_transformed,Abi,Abi1,Tbox) :-
 	nl,
 	write('Entrez le nom du deuxieme concept:'),
 	nl,
 	read(C2),
 	recc_replace(C2,C2_transformed),
 	validate_concept(C2_transformed),
-	nnf(and(C1,C2),Prop_to_add_in_Abox),
-	concat(Abi,[some(X,Prop_to_add_in_Abox)],Abi1).
+	nnf(and(C1_transformed,C2_transformed),Prop_to_add_in_Abox),
+	concat(Abi,[inst(X,Prop_to_add_in_Abox)],Abi1).
 /*
  *
  *Verification sur l'instance
@@ -101,7 +104,9 @@ concept(all(R,C)) :- rname(R),concept(C).
 
 /*
  * fonction de remplacement reccuressive
+	test : sculpteur, editeur
  */
+
 recc_replace(C_origin, C_origin) :- concept(C_origin).
 recc_replace(and(C1,C2),C_target) :- recc_replace(C1,C_temp1), recc_replace(C2,C_temp2), recc_replace(and(C_temp1,C_temp2),C_target).
 recc_replace(or(C1,C2),C_target) :- recc_replace(C1,C_temp1), recc_replace(C2,C_temp2), recc_replace(or(C_temp1,C_temp2),C_target).
@@ -110,20 +115,23 @@ recc_replace(some(R,C_origin),C_target) :- recc_replace(C_origin,C_temp), recc_r
 recc_replace(some(R,C_origin),C_target) :- recc_replace(C_origin,C_temp), recc_replace(all(R,C_temp),C_target).
 recc_replace(C_origin,C_target) :- equiv(C_origin,X), recc_replace(X,C_target).
 
-literal(X) :- cnamea(X).
-literal(X) :- not(Y) = X , literal(Y).
 
 troisieme_etape(Abi,Abr) :-
+	/*simplify_Abox(Abi),*/
 	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
-  resolution(Lie,Lpt,Li,Lu,Ls,Abr),
+	resolution(Lie,Lpt,Li,Lu,Ls,Abr),
 	nl,
 	write('Youpiiiiii, on a demontre la proposition initiale !!!').
 
+literal(X) :- cnamea(X).
+literal(X) :- not(Y) = X , literal(Y).
+
+
 /*traitement des litteraux*/
 tri_Abox([],[],[],[],[],[]).
-tri_Abox([(I,C)],Lie,Lpt,Li,Lu,[(I,C)|Ls]):-
+tri_Abox([inst(I,C)],Lie,Lpt,Li,Lu,[(I,C)|Ls]):-
 	tri_Abox([],Lie,Lpt,Li,Lu,Ls), literal(C), iname(I).
-tri_Abox([(I,C)|Abi],Lie,Lpt,Li,Lu,[(I,C)|Ls]):-
+tri_Abox([inst(I,C)|Abi],Lie,Lpt,Li,Lu,[(I,C)|Ls]):-
 	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
 	iname(I),
 	literal(C).
@@ -136,10 +144,10 @@ tri_Abox([inst(I,some(R,C))|Abi],[(I,some(R,C))|Lie],Lpt,Li,Lu,Ls):-
 	iname(I), concept(C), rname(R).
 
 /*traitement quelque soit*/
-tri_Abox([inst(I,all(R,C))],[(I,all(R,C))|Lie],Lpt,Li,Lu,Ls):-
+tri_Abox([inst(I,all(R,C))],Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls):-
 	tri_Abox([],Lie,Lpt,Li,Lu,Ls),
 	iname(I), concept(C), rname(R).
-tri_Abox([inst(I,all(R,C))|Abi],[(I,all(R,C))|Lie],Lpt,Li,Lu,Ls):-
+tri_Abox([inst(I,all(R,C))|Abi],Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls):-
 	tri_Abox(Abi,Lie,Lpt,Li,Lu,Ls),
 	iname(I), concept(C), rname(R).
 
@@ -157,10 +165,68 @@ tri_Abox([inst(I,or(C1,C2))|Abi],Lie,Lpt,Li,[inst(I,or(C1,C2))|Lu],Ls):-
 
 
 resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	/*test_clash()*/
+	Lie \== [],
 	complete_some(Lie,Lpt,Li,Lu,Ls,Abr).
-complete_some(Lie,Lpt,Li,Lu,Ls,Abr) :-
-	write(Lie),
-	write(Lpt),
-	write(Li),
-	write(Lu),
-	write(Ls).
+	/*evolution
+	affiche_evolution
+	resolution
+	*/
+
+/*resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	test_clash()
+	transforamtion_and(Lie,Lpt,Li,Lu,Ls,Abr),
+	evolution
+	affiche_evolution
+	resolution
+	*/
+
+resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	/*test_clash()*/
+	Lpt \==[],
+	deduction_all(Lie,Lpt,Li,Lu,Ls,Abr).
+	/*evolution
+	affiche_evolution
+	resolution
+	*/
+
+/*resolution(Lie,Lpt,Li,Lu,Ls,Abr) :-
+	test_clash()
+	transforamtion_or(Lie,Lpt,Li,Lu,Ls,Abr),
+	evolution ( va se charger de supprimer le preùier element de chaque liste)
+	affiche_evolution
+	resolution
+	*/
+
+/*test david, all(aEcrit,livre)*/
+complete_some([],Lpt,Li,Lu,Ls,Abr).
+
+complete_some([(I,some(R,C))],Lpt,Li,Lu,Ls,Abr) :-
+	complete_some([],Lpt,Li,Lu,Ls_after,Abr_after),
+	concept(C), rname(R), iname(I), genere(B),
+	concat([instR(I,B,R)],Abr,Abr_after),
+	concat([(B,C)],Ls,Ls_after).
+
+complete_some([(I,some(R,C))|Lie],Lpt,Li,Lu,Ls,Abr) :-
+	complete_some(Lie,Lpt,Li,Lu,Ls_after,Abr_after),
+	concept(C), rname(R), iname(I), genere(B),
+	concat([instR(I,B,R)],Abr,Abr_after),
+	concat([(B,C)],Ls,Ls_after).
+
+/*test michelAnge, some(aCree,sculpture)*/
+deduction_all(Lie,[],Li,Lu,Ls,Abr).
+
+deduction_all(Lie,[(I,all(R,C))],Li,Lu,Ls,Abr) :-
+	member(instR(I,B,R), Abr), iname(I),iname(B),concept(C),rname(R),
+	deduction_all(Lie,[],Li,Lu,Ls_after,Abr_after),
+	enleve(instR(I,B,R), Abr,Abr_after),
+	concat([(B,C)],Ls,Ls_after).
+
+deduction_all(Lie,[(I,all(R,C))|Lpt],Li,Lu,Ls,Abr) :-
+	member((I,B,R), Abr), iname(I),iname(B),concept(C),rname(R),
+	deduction_all(Lie,Lpt,Li,Lu,Ls_after,Abr_after),
+	enleve((I,B,R), Abr,Abr_after),
+	concat([(B,C)],Ls,Ls_after).
+	
+
+evolue((2,B,C),Lie,Lpt,Li,Lu,Ls,Lie1,Lpt1,Li1,Lu1,Ls1) :-
