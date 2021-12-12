22ffba7a4de6eac1819f9ec81e0c6a97fedccc47
Balkis <60971348+balkisdirahoui@users.noreply.github.com>
2021-12-12 01:03:12 +0100

    merged

--- test.pl
+++ test.pl
@@ -1,5 +1,128 @@
+/*
 :-[box].
 :-[utils].
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***************************************************BOX**********************************************/
+equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(auteur,and(personne,some(aEcrit,livre))).
+equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))).
+equiv(parent,and(personne,some(aEnfant,anything))).
+
+cnamea(personne).
+cnamea(livre).
+cnamea(objet).
+cnamea(sculpture).
+cnamea(anything).
+cnamea(nothing).
+cnamena(auteur).
+cnamena(editeur).
+cnamena(sculpteur).
+cnamena(parent).
+iname(michelAnge).
+iname(david).
+iname(sonnets).
+iname(vinci).
+iname(joconde).
+rname(aCree).
+rname(aEcrit).
+rname(aEdite).
+rname(aEnfant).
+inst(michelAnge,personne).
+inst(sonnets,livre).
+inst(vinci,personne).
+inst(joconde,objet).
+instR(michelAnge, david, aCree).
+instR(michelAnge, sonnets, aEcrit).
+instR(vinci, joconde, aCree).
+/***************************************************UTILS**********************************************/
+concat([],L1,L1).
+concat([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
+
+enleve(X,[X|L],L) :-!.
+enleve(X,[Y|L],[Y|L2]) :- enleve(X,L,L2).
+
+
+genere(Nom) :-
+		compteur(V),nombre(V,L1),
+		concat([105,110,115,116],L1,L2),
+		V1 is V+1,
+		dynamic(compteur/1),
+		retract(compteur(V)),
+		dynamic(compteur/1),
+		assert(compteur(V1)),nl,nl,nl,
+         	name(Nom,L2).
+nombre(0,[]).
+nombre(X,L1) :-
+		R is (X mod 10),
+		Q is ((X-R)//10),
+		chiffre_car(R,R1),
+		char_code(R1,R2),
+		nombre(Q,L),
+		concat(L,[R2],L1).
+chiffre_car(0,'0').
+chiffre_car(1,'1').
+chiffre_car(2,'2').
+chiffre_car(3,'3').
+chiffre_car(4,'4').
+chiffre_car(5,'5').
+chiffre_car(6,'6').
+chiffre_car(7,'7').
+chiffre_car(8,'8').
+chiffre_car(9,'9').
+
+lecture([X|L]):-read(X), X \= fin, !,lecture(L).
+lecture([]).
+
+nnf(not(and(C1,C2)),or(NC1,NC2)):- nnf(not(C1),NC1),
+nnf(not(C2),NC2),!.
+nnf(not(or(C1,C2)),and(NC1,NC2)):- nnf(not(C1),NC1),
+nnf(not(C2),NC2),!.
+nnf(not(all(R,C)),some(R,NC)) :- nnf(not(C),NC),!.
+nnf(not(some(R,C)),all(R,NC)):- nnf(not(C),NC),!.
+nnf(not(not(X)),X):-!.
+nnf(not(X),not(X)):-!.
+nnf(and(C1,C2),and(NC1,NC2)):- nnf(C1,NC1),nnf(C2,NC2),!.
+nnf(or(C1,C2),or(NC1,NC2)):- nnf(C1,NC1), nnf(C2,NC2),!.
+nnf(some(R,C),some(R,NC)):- nnf(C,NC),!.
+nnf(all(R,C),all(R,NC)) :- nnf(C,NC),!.
+nnf(X,X).
+unique([],[]).
+unique([A],[A]).
+unique([A,B|C],D) :- delete(A,[B|C],E),
+unique(E,F),
+append(A,F,D).
+
+append(A,B,B) :- member(A,B),!.
+append(A,B,[A|B]).
+
+
+ delete(_,[],[]).
+delete(A,[A],[]).
+delete(A,[A|B],B).
+ delete(A,[B|C],[B|D]) :- delete(A,C,D), !.
+
+/***************************************************TEST**********************************************/
 compteur(1).
 
 prog :-
@@ -14,14 +137,12 @@ premiere_etape(Tbox,Abi,Abr) :-
 	setof(instR(X,Y,Z), instR(X,Y,Z),Abr).
 
 
-%autoref((C,all(R,C2)),Tbox) :- write(C),nl,write(R)
-%,nl,write(C2),[(C2,some(R,C3))|Tbox], +\ C2 ==C3,!,autoref((C,all(R,C2)),Tbox) .
-%autoref((C,D),[]). 
+
 	
 	
 	
 	
-	/*****************************************************/
+/*****************************************************/
 /*Partie 2 : Validation des propositions*/
 /*****************************************************/
 deuxieme_etape(Abi,Abi1,Tbox) :-
