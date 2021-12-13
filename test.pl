a1840267074ba4056fc29747597f2b3ba0f832a4
Balkis <60971348+balkisdirahoui@users.noreply.github.com>
2021-12-13 01:00:00 +0100

    box

--- test.pl
+++ test.pl
@@ -1,4 +1,36 @@
-:- [box].
+/***************************************************BOX*********************************************/
+equiv(sculpteur,and(personne,some(aCree,sculpture))).
+equiv(auteur,and(personne,some(aEcrit,livre))).
+equiv(editeur,and(personne,and(not(some(aEcrit,livre)),some(aEdite,livre)))).
+equiv(parent,and(personne,some(aEnfant,anything))).
+
+cnamea(personne).
+cnamea(livre).
+cnamea(objet).
+cnamea(sculpture).
+cnamea(anything).
+cnamea(nothing).
+cnamena(auteur).
+cnamena(editeur).
+cnamena(sculpteur).
+cnamena(parent).
+iname(michelAnge).
+iname(david).
+iname(sonnets).
+iname(vinci).
+iname(joconde).
+rname(aCree).
+rname(aEcrit).
+rname(aEdite).
+rname(aEnfant).
+inst(michelAnge,personne).
+inst(sonnets,livre).
+inst(vinci,personne).
+inst(joconde,objet).
+instR(michelAnge, david, aCree).
+instR(michelAnge, sonnets, aEcrit).
+instR(vinci, joconde, aCree).
+
 /***************************************************UTILS**********************************************/
 concat([],L1,L1).
 concat([X|Y],L1,[X|L2]) :- concat(Y,L1,L2).
